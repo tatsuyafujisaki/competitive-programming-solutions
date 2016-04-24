c4f2a36c338721d9da73d7f1407fe1ac31af5793
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-04-24 12:16:24 +0900

    Add ProblemB-Revenge-of-the-Pancakes.cs

---
+++ Google Code Jam 2016/Qualification Round/ProblemB-Revenge-of-the-Pancakes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Gcj
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            const string inputFile = @"B-large-practice.in";
+            const string outputFile = @"output.txt";
+
+            var xs = File.ReadLines(inputFile).ToList();
+            var n = int.Parse(xs[0]);
+            var s = xs.GetRange(1, n).ToList();
+
+            var result = new List<string>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var groupedHeight = 1 + CountSubstring(s[i], "-\\+") + CountSubstring(s[i], "\\+-");
+                result.Add($"Case #{i + 1}: {(s[i].EndsWith("-") ? groupedHeight : groupedHeight - 1)}");
+            }
+
+            File.WriteAllLines(outputFile, result);
+        }
+
+        private static int CountSubstring(string s, string substring)
+        {
+            return Regex.Matches(s, substring, RegexOptions.IgnoreCase).Count;
+        }
+    }
+}
